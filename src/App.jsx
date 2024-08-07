6a6140ac4c1debddb6c5be93eabf5f18fb5a9b75
Cody Kremer <ckremer99@outlook.com>
2024-08-06 17:06:43 -0700

    finished exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import * as weatherService from './services/weatherService';
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [weather, setWeather] = useState()
+
+  const fetchData = async () => {
+    const data = await weatherService.show('New York');
+    console.log('Data:', data);
+  };
+
+  useEffect(() => {
+
+    const fetchDefaultData = async () => {
+      const data = await weatherService.show('New York');
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    };
+
+    // Call the fetch function when the page loads:
+    fetchDefaultData();
+
+  }, []);
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
-}
+};
 
 export default App
---
+++ src/services/weatherService.js
@@ -0,0 +1,16 @@
+const API_KEY = "b71f7d96c246495ba90224146240608"
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+      const queryString = `&q=${city}`;
+      const res = await fetch(BASE_URL + queryString);
+      const data = await res.json();
+      console.log('Data:', data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  export { show }
